5fb7b63c69feebb43aa2762e093e9d2b86778a73
PiousGamer <99356391+PiousGamer@users.noreply.github.com>
2022-04-06 15:57:05 -0700

    Create Head_Over_Heels.rb

---
+++ Head_Over_Heels.rb
@@ -0,0 +1,401 @@
+#Head Over Heels
+use_bpm 100
+use_synth :piano
+use_synth_defaults amp: 2
+
+
+play:g5
+play:g4
+sleep 1
+play:fs5
+play:fs4
+sleep 0.5
+play:e5
+play:e4
+sleep 0.5
+play:d5
+play:d4
+sleep 1
+play:e5
+play:e4
+sleep 0.5
+play:c5
+play:c4
+sleep 0.5
+
+play:c5
+play:c4
+sleep 0.5
+play:g4
+play:g5
+sleep 0.5
+play:fs5
+play:fs4
+sleep 0.5
+play:e5
+play:e4
+sleep 0.5
+play :d4
+play :d5
+sleep 0.5
+play :e5
+play :e4
+sleep 0.5
+play:c5
+play:c4
+sleep 1
+
+play :r
+sleep 1
+play :r
+sleep 0.5
+play :a4
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :cs5
+sleep 0.5
+play :a4
+sleep 0.5
+
+play :g5
+sleep 0.5
+play :a5
+sleep 0.5
+play :b5
+sleep 0.5
+
+play :g5
+sleep 0.5
+play :r
+sleep 0.5
+play :r
+sleep 1
+
+play :r
+sleep 1
+play :r
+sleep 0.5
+play :a4
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :cs5
+sleep 0.5
+play :a4
+sleep 0.5
+
+play :e5
+sleep 0.5
+play :a4
+sleep 0.5
+play :r
+sleep 1
+play :r
+sleep 1
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+
+play :e5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :cs5
+sleep 0.5
+play :e5
+sleep 0.5
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+
+play :g5
+sleep 0.5
+play :a5
+sleep 0.5
+play :b5
+sleep 0.5
+play :g5
+sleep 0.5
+play :r
+sleep 2
+
+play :r
+sleep 1
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :cs5
+sleep 0.5
+play :e5
+sleep 0.5
+
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+play :r
+sleep 1
+play :r
+sleep 2
+
+play :r
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :cs5
+sleep 0.5
+play :e5
+sleep 0.5
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+
+play :g5
+sleep 0.5
+play :a5
+sleep 0.5
+play :b5
+sleep 0.5
+play :g5
+sleep 0.5
+play :r
+sleep 0.5
+play :r
+sleep 1
+
+play :r
+sleep 1
+play :r
+sleep 0.5
+play :a4
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :cs5
+sleep 0.5
+play :e5
+sleep 0.5
+
+2.times do
+  play :a4
+  sleep 0.5
+end
+play :r
+sleep 1
+play :r
+sleep 1
+play :r
+sleep 0.5
+play :a4
+sleep 0.5
+
+play :e5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :cs5
+sleep 0.5
+play :e5
+sleep 0.5
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+
+play :g5
+sleep 0.5
+play :a5
+sleep 0.5
+play :b5
+sleep 0.5
+play :g5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+
+play :cs5
+sleep 0.5
+play :d5
+sleep 0.5
+play :e5
+sleep 0.5
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+
+
+play :r
+sleep 0.5
+play :c5
+sleep 0.5
+play :r
+sleep 0.5
+play :c5
+sleep 0.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.5
+play :g4
+sleep 0.5
+
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+
+play :d5
+sleep 0.5
+play :b4
+sleep 0.5
+play :d5
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :fs5
+sleep 0.5
+play :fs5
+sleep 0.5
+play :e5
+sleep 0.5
+play :b4
+sleep 0.5
+play :r
+sleep 0.5
+play :e5
+sleep 0.5
+play :e5
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :d5
+sleep 0.5
+play :b4
+sleep 0.5
+play :d5
+sleep 0.5
+play :r
+sleep 1
+play :d5
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :fs5
+sleep 0.5
+play :fs5
+sleep 0.5
+play :g5
+sleep 0.5
+play :a5
+sleep 0.5
+play :a5
+sleep 0.5
+play :b5
+sleep 0.5
+play :g5
+sleep 0.5
+
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :b4
+sleep 0.5
+play :d5
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :fs5
+sleep 0.5
+play :fs5
+sleep 0.5
+play :e5
+sleep 0.5
+play :b4
+sleep 0.5
+play :b4
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+
+play :fs5
+sleep 0.5
+play :fs5
+sleep 0.5
+play :e5
+sleep 0.5
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :d5
+sleep 0.5
+play :c5
+sleep 0.5
+play :c5
+sleep 0.5
+
+play :r
+sleep 0.5
+play :c5
+sleep 0.5
+play :r
+sleep 0.5
+play :c5
+sleep 0.5
+play :b4
+sleep 0.5
+play :b4
+sleep 0.5
+play :a4
+sleep 0.5
+play :a4
+sleep 0.5
